f5b7f38b1151c16409daf2dbc51b7ee84bf82835
leokapsy <leokapfy@gmail.com>
2022-09-19 22:36:36 +0200

    swap

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -8,12 +8,11 @@
  * Return: Always 0.
  */
 void swap_int(int *a, int *b)
-{
-	int a, b;
 
+{
 	a = 98;
 	b = 42;
-	printf('a=%d, b=%d\n", a, b)
+	printf("a=%d, b=%d\n", a, b);
 	swap_int(&a, &b);
 	printf("a=%d, b=%d\n", a, b);
 	return (0);

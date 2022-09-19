efb37bef6412b76a71ce9d7834dec6f0af133f09
leokapsy <leokapfy@gmail.com>
2022-09-19 22:12:22 +0200

    swap

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * swap_int - function that swaps the values of two integers
+ *@a: pointer
+ *@b: pointers
+ * Return: Always 0.
+ */
+void swap_int(int *a, int *b)
+{
+	int a, b;
+
+	a = 20;
+	b = 30;
+	printf('a=%d, b=%d\n", a, b)
+	swap_int(&a, &b);
+	printf("a=%d, b=%d\n", a, b);
+	return (0);
+}
